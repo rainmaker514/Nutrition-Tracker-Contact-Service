c20b2adb8d3f0d20bde7613892e88ddeefedf924
rainmaker514 <tdoug016@fiu.edu>
2021-10-17 11:03:34 -0400

    added repo

---
+++ Nutrition-Tracker-Contact-Service/Nutrition-Tracker-Contact-Service/src/main/java/com/nutritiontracker/NutritionTrackerContactService/repo/ContactRepository.java
@@ -0,0 +1,12 @@
+package com.nutritiontracker.NutritionTrackerContactService.repo;
+
+import com.nutritiontracker.NutritionTrackerContactService.model.Contact;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ContactRepository extends JpaRepository<Contact, Long> {
+    void deleteUserById(Long id);
+
+    Contact findUserById(Long id);
+}
