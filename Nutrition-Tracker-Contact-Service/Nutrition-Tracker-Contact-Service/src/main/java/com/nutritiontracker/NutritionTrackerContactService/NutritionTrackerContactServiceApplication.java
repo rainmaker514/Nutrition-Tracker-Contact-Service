22d9b63a10aacb2aed3932937fbfb7cd178919a1
rainmaker514 <tdoug016@fiu.edu>
2021-10-17 01:19:14 -0400

    initial

---
+++ Nutrition-Tracker-Contact-Service/Nutrition-Tracker-Contact-Service/src/main/java/com/nutritiontracker/NutritionTrackerContactService/NutritionTrackerContactServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nutritiontracker.NutritionTrackerContactService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutritionTrackerContactServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutritionTrackerContactServiceApplication.class, args);
+	}
+
+}
